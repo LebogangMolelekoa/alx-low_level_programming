bbc07b4ad3edefbf1cd57c9c80628ade02318465
LebogangMolelekoa <lebogang.m@icloud.com>
2023-10-26 15:44:40 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,12 +11,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int setbit;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if(index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	if
-	{
-		setbit = 1 << index;
-		*n = *n | setbit;
-	}
+	setbit = 1 << index;
+	*n = *n | setbit;
 	return (1);
 }
